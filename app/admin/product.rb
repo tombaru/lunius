0349871772e30dd65e32e59125981f049d57f3ed
Pasha <goblin@goblin.(none)>
2014-02-11 17:45:20 +0400

    new model Product (index_page)

---
+++ app/admin/product.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Product do
+
+  
+  # See permitted parameters documentation:
+  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # permit_params :list, :of, :attributes, :on, :model
+  #
+  # or
+  #
+  permit_params do
+    [:name, :price]
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Lunius::Application.routes.draw do
+  get "products/index"
   ActiveAdmin.routes(self)
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
