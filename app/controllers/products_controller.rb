3a0dfa9fbce094a49a40c5fd76a9875fb22f6f56
Pasha <goblin@goblin.(none)>
2014-02-13 15:20:37 +0400

    new attribute Show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,22 @@
 class ProductsController < ApplicationController
-
+  before_action :set_product, only: [:show]
   def index
     @products = Product.all
   end
+
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def product_params
+      params[:product].permit :name, :price
+    end
+
 
 end
