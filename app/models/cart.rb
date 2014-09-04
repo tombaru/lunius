b799196c281a62f7c8080ac5dfd6290dbf3610ed
Pasha <goblin@goblin.(none)>
2014-09-04 17:44:28 +0400

    fix add products to cart from product list

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,7 @@ class Cart < ActiveRecord::Base
 	def add_product(product_id, quantity)
 			current_item = line_items.find_by_product_id(product_id)
 			if current_item.present?
-					current_item.quantity += quantity.to_i
+					current_item.quantity += quantity.to_i || 1
 			else
 					current_item = line_items.build(product_id: product_id, quantity: quantity || 1)
 			end
